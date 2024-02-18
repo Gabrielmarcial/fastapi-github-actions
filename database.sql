353393f4b4302fb4a9d12b73bc65215c1930999f
Gabrielmarcial <gmpaiva228@gmail.com>
2024-02-18 11:13:26 -0300

    include do database.sql e sript de insert

--- database.sql
+++ database.sql
@@ -1,6 +1,3 @@
--- DROP SCHEMA public;
-
-CREATE SCHEMA public AUTHORIZATION pg_database_owner;
 
 -- DROP SEQUENCE public.eletronicos_id_seq;
 
@@ -24,3 +21,9 @@ CREATE TABLE public.eletronicos (
 	preco numeric(10, 2) NULL,
 	CONSTRAINT eletronicos_pkey PRIMARY KEY (id)
 );
+
+INSERT INTO eletronicos (nome, marca, modelo, preco) VALUES ('Smartphone', 'Apple', 'iPhone 13 Pro', 1099.99);
+INSERT INTO eletronicos (nome, marca, modelo, preco) VALUES ('Laptop', 'Dell', 'XPS 13', 1299.99);
+INSERT INTO eletronicos (nome, marca, modelo, preco) VALUES ('Televisão', 'Samsung', 'QLED 4K', 999.99);
+INSERT INTO eletronicos (nome, marca, modelo, preco) VALUES ('Tablet', 'Samsung', 'Galaxy Tab S7', 649.99);
+INSERT INTO eletronicos (nome, marca, modelo, preco) VALUES ('Câmera Digital', 'Canon', 'EOS Rebel T7i', 599.99);
